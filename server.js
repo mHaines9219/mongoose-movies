6c77e0f00a693c0ee17305d07c13ede8220c75c9
mhaines9219 <mhaines9219@gmail.com>
2023-10-24 11:10:53 -0700

    configure passport

--- server.js
+++ server.js
@@ -1,52 +1,64 @@
-var createError = require('http-errors');
-var express = require('express');
-var path = require('path');
-var cookieParser = require('cookie-parser');
-var logger = require('morgan');
+var createError = require("http-errors");
+var express = require("express");
+var path = require("path");
+var cookieParser = require("cookie-parser");
+var logger = require("morgan");
+var session = require("express-session");
+var passport = require("passport");
 
-require('dotenv').config();
+require("dotenv").config();
 // connect to the database with AFTER the config vars are processed
-require('./config/database');
+require("./config/database");
+//config passport middleware
+require("/.config/passport");
 
-const indexRouter = require('./routes/index');
-const moviesRouter = require('./routes/movies');
-const reviewsRouter = require('./routes/reviews');
-const performersRouter = require('./routes/performers');
-
+const indexRouter = require("./routes/index");
+const moviesRouter = require("./routes/movies");
+const reviewsRouter = require("./routes/reviews");
+const performersRouter = require("./routes/performers");
 
 var app = express();
 
 // view engine setup
-app.set('views', path.join(__dirname, 'views'));
-app.set('view engine', 'ejs');
+app.set("views", path.join(__dirname, "views"));
+app.set("view engine", "ejs");
 
-app.use(logger('dev'));
+app.use(logger("dev"));
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 app.use(cookieParser());
-app.use(express.static(path.join(__dirname, 'public')));
+app.use(express.static(path.join(__dirname, "public")));
+app.use(
+  session({
+    secret: process.env.SECRET,
+    resave: false,
+    saveUninitialized: true,
+  })
+);
+app.use(passport.initialize());
+app.use(passport.session());
 
-app.use('/', indexRouter);
-app.use('/movies', moviesRouter);
-// Mount these routers to root because not all 
+app.use("/", indexRouter);
+app.use("/movies", moviesRouter);
+// Mount these routers to root because not all
 // paths for a related/nested resource begin the same
-app.use('/', reviewsRouter);
-app.use('/', performersRouter);
+app.use("/", reviewsRouter);
+app.use("/", performersRouter);
 
 // catch 404 and forward to error handler
-app.use(function(req, res, next) {
+app.use(function (req, res, next) {
   next(createError(404));
 });
 
 // error handler
-app.use(function(err, req, res, next) {
+app.use(function (err, req, res, next) {
   // set locals, only providing error in development
   res.locals.message = err.message;
-  res.locals.error = req.app.get('env') === 'development' ? err : {};
+  res.locals.error = req.app.get("env") === "development" ? err : {};
 
   // render the error page
   res.status(err.status || 500);
-  res.render('error');
+  res.render("error");
 });
 
 module.exports = app;
